1e3cd53298bbde5112538298326b1e908e6f71e7
devronaldev <ronald.pereira.evangelista@gmail.com>
2025-11-17 00:43:39 -0300

    Feat: Domain now is vanishing its logs after hundred or more are registered.

--- TP11-EDD2.DATA/Entities/Domain.cs
+++ TP11-EDD2.DATA/Entities/Domain.cs
@@ -3,11 +3,27 @@ namespace TP11_EDD2.DATA.Entities;
 public class Domain
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string? Name { get; set; }
     public Queue<Log> Logs { get; set; }
+
+    public Domain()
+    {
+        Logs = new Queue<Log>();
+    }
+
+    public Domain(int id, string name) : this()
+    {
+        Id = id;
+        Name = name;
+    }
     
     public void RegistLog(Log log)
     {
+        if (Logs.Count == 100)
+        {
+            Logs.Dequeue();
+        }
+
         Logs.Enqueue(log);
     }
 }
