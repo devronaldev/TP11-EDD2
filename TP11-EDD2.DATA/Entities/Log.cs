ddbb254b9c1d89eceebccc37bdabb06803836463
devronaldev <ronald.pereira.evangelista@gmail.com>
2025-11-17 00:44:06 -0300

    Feat: Log.cs now has constructor function.

--- TP11-EDD2.DATA/Entities/Log.cs
+++ TP11-EDD2.DATA/Entities/Log.cs
@@ -4,5 +4,12 @@ public class Log
 {
     public DateTime AccessDate { get; set; }
     public User User { get; set; }
-    public bool isAuthorized { get; set; }
+    public bool IsAuthorized { get; set; }
+
+    public Log(User user, bool isAuthorized)
+    {
+        AccessDate = DateTime.UtcNow;
+        User = user;
+        IsAuthorized = isAuthorized; 
+    }
 }
